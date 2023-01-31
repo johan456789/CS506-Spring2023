5534629131f7bd56725ca823527eaedece7c1200
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2023-01-31 16:09:12 -0500

    add bu

--- labs/01-lab/education/school.py
+++ labs/01-lab/education/school.py
@@ -1,3 +1,52 @@
-def draw_school():
-    print("school not found")
-    return
+print(r"""
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMMMMMMMMMMMMMMWWWWWWWWWWWWWWWWWWMMMMMWWWWWWWWWWWWWWWWWWWWMMMMMM
+MMMMKdlllllllllllllllllllllllllllllllokXWMMMMMW0ollllllllllllllllllllo0WMXdlllllllllllllllllllo0WMMM
+MMMWk,................................';o0WMMMWx'.....................dWWO;....................dWMMM
+MMMWk,...................................,dXWMWx'.....................dWMO;....................dWMMM
+MMMWk,.....................................;xXWx'.....................dWWO;....................dWMMM
+MMMWk,......................................;OWx'.....................dWMO;....................dWMMM
+MMMWk,......................................;OWx'.....................dWWO;....................dWMMM
+MMMMXkxxd:...........;ddxxxxxxxxo,..........;OWKxxxxd;...........'lxxxKWMXkxxd:...........;ddxxKWMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc..........;OMMMMMMNo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc..........;OMMMMMMNo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc..........;OMMMMMMNo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc..........;OMMMMMMNo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oXWWWWWWWWWK:..........;OMMMMMMNo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........,:ccccccccc:'..........cKMWXNWWNo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd................................;xXNOl:OWWNo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...............................:k0d;'c0WMMNo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...............................,,...lKXXXNXo...........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd............................... ...,odoloodc'..........;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd..............................  ...'....'',:odo:'......;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........:xkkkkkkkkkd;........ ...       .'oKXk:'....;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc........   ...    .'::oXW0:....;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc........ ....,;..  .ckKNMW0:...;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc.......  ..  .col;...'lOKKOc...;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc.......     'oxolool;'':dd:,'..;0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc.........   .cc'.;dxc;;cdxoc;'.:0MMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc............','...';lOXXKkdolcco0WMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMMMMMWd...........oNMMMMMMMMMKc.......';:cxkd;..:kXWMMWXko'..;xKNMMMMMMMMMWx'..........oNMMMMMMMM
+MMMMNXKK0l...........c0KKKKKKKKKk;...'';cl:ck00OkxOKWMMMMMMMWOlo0XO0KKKKKKKKK0o'..........oNMMMMMMMM
+MMMWO:,,'.............',,,,,,,,,'....',coo;cOO00OkxONWNNWMMMMMNWWO:',,,,,,,,,,'...........oNMMMMMMMM
+MMMWk,.................................',::cdO0kxdoxKNNWMMMMWX0kx:'.......................oNMMMMMMMM
+MMMWk,..............................      .cllcclclcd0XNNX0xlc;'.........................'xNMMMMMMMM
+MMMWk,..............................   .':lxkocoo;'';lc:;;,'....''.....................'cONMMMMMMMMM
+MMMWk,...............................  ;OKNNNNNNXOxolol'..;,.........................'cONMMMMMMMMMMM
+MMMWk,.................................ckOKKNMMMMMWXxdxl'.';'......................'cONMMMMMMMMMMMMM
+MMMMXOxxxxxxxxkkxxxxxxxxxxxxxxxxxxxxkxdoc;cdKWMMMWNk'......,ldkkxxxxxxxxxkkkxxxkkkkOXNNWWWWMMMMMMMMM
+MMMMKo:::c:::::::::::::::::::::c::c::cc:,...;dO0kl:'      .;c;c:::::c:::cccc:::ccc:cc:cc:lOWMMMMMMMM
+MMMWO;..';;,'',;'',;,',;;;,';,',,,;,'..',;,,'',..   .'',''',,;;,',;;,',;;',,,,;;;,,,,;,'.'xWMMMMMMMM
+MMMWO;..:kkxllxkdlxkdodkkkookxolOkkd,..;xxdxccxl:;.'locxkkkloxxdclxxxloddllolxkkkooxkkl'.'xWMMMMMMMM
+MMMWO;..:kOkodxxkoddoc;llc:xxxdl0K0d'..,xdox:cOKKd,,lc'lOOo,lxlc;cdxkllloc:l:;ll:,,oOo,..'xWMMMMMMMM
+MMMWO;..ckOkodkOxloxkc'cd;,dkkdo0KKd'..,dkxx:cO0Kd,;oo';xk:'oxdxclxkxcldkold;'lo,..cxc...'xWMMMMMMMM
+MMMWO;..;clc,;cc;,:l:,';c,';cc;;c::;'..';cc:';c::;',::'';;'':llc;::::::lc;:c,'::,..;c;...'xWMMMMMMMM
+MMMW0:.'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''',kWMMMMMMMM
+MMMMNKOOOOO000OOOOOOO00OO00OOOOOO0000OOO0OOO00OOOOOOOOOOO00OOOOOO000OOOOOOOOOOO0OOOO000O00NMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+""")
